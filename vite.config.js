41a48341888f6bda3c89f3056d7124af1b47bb8f
Kevin Lo <k.a.l.o.gncd@gmail.com>
2024-12-17 18:35:51 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,12 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'public/dist', // Change output directory to 'public/dist'
+        emptyOutDir: true, // Clean old files on build
+    },
 });
